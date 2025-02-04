81a6b7f4a230fd66c3522b9b199667e0fa1cbbe4
Tilly Woodfield <22456167+tillywoodfield@users.noreply.github.com>
2025-02-04 11:34:09 +0200

    feat: download datasets

--- oc4ids_datastore_pipeline/pipeline.py
+++ oc4ids_datastore_pipeline/pipeline.py
@@ -1,7 +1,45 @@
 import logging
+from typing import Any
+
+import requests
 
 logger = logging.getLogger(__name__)
 
+REGISTERED_DATASETS = {
+    "uganda_gpp": "https://gpp.ppda.go.ug/adminapi/public/api/open-data/v1/infrastructure/projects/download?format=json",  # noqa: E501
+    "ghana_cost_sekondi_takoradi": "https://costsekondi-takoradigh.org/uploads/projectJson.json",  # noqa: E501
+    "mexico_cost_jalisco": "http://www.costjalisco.org.mx/jsonprojects",
+    "mexico_nuevo_leon": "http://si.nl.gob.mx/siasi_ws/api/edcapi/DescargarProjectPackage",  # noqa: E501
+    "indonesia_cost_west_lombok": "https://intras.lombokbaratkab.go.id/oc4ids",
+    "ukraine_cost_ukraine": "https://portal.costukraine.org/data.json",
+    "malawi_cost_malawi": "https://ippi.mw/api/projects/query",
+}
+
+
+def download_json(url: str) -> Any:
+    logger.info(f"Downloading json from {url}")
+    try:
+        r = requests.get(url)
+        r.raise_for_status()
+        response_size = len(r.content)
+        logger.info(f"Downloaded {url} ({response_size} bytes)")
+        return r.json()
+    except Exception as e:
+        raise Exception("Download failed", e)
+
+
+def process_dataset(dataset_name: str, dataset_url: str) -> None:
+    logger.info(f"Processing dataset {dataset_name}")
+    try:
+        download_json(dataset_url)
+    except Exception as e:
+        logger.warning(f"Failed to process dataset {dataset_name} with error {e}")
+
+
+def process_datasets() -> None:
+    for name, url in REGISTERED_DATASETS.items():
+        process_dataset(name, url)
+
 
 def run() -> None:
-    logger.info("Hello World!")
+    process_datasets()
--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -1,2 +1,24 @@
-def test_hello_world() -> None:
-    pass
+import pytest
+from pytest_mock import MockerFixture
+
+from oc4ids_datastore_pipeline.pipeline import download_json, process_dataset
+
+
+def test_download_json_raises_failure_exception(mocker: MockerFixture) -> None:
+    patch_get = mocker.patch("oc4ids_datastore_pipeline.pipeline.requests.get")
+    patch_get.side_effect = Exception("Mocked exception")
+
+    with pytest.raises(Exception) as exc_info:
+        download_json(url="https://test_dataset.json")
+
+    assert "Download failed" in str(exc_info.value)
+    assert "Mocked exception" in str(exc_info.value)
+
+
+def test_process_dataset_catches_exception(mocker: MockerFixture) -> None:
+    patch_download_json = mocker.patch(
+        "oc4ids_datastore_pipeline.pipeline.download_json"
+    )
+    patch_download_json.side_effect = Exception("Download failed")
+
+    process_dataset("test_dataset", "https://test_dataset.json")
