59b8da62dcd01965e08b0293b3f1581692e38f39
Tilly Woodfield <22456167+tillywoodfield@users.noreply.github.com>
2025-02-06 13:24:27 +0200

    test: fix env loading in CI runner

--- oc4ids_datastore_pipeline/__init__.py
+++ oc4ids_datastore_pipeline/__init__.py
@@ -15,4 +15,4 @@ logger = logging.getLogger(__name__)
 
 APP_ENV = os.environ.get("APP_ENV", "local")
 logger.info(f"Loading {APP_ENV} environment variables")
-load_dotenv(find_dotenv(f".env.{APP_ENV}"))
+load_dotenv(find_dotenv(f".env.{APP_ENV}", usecwd=True))
